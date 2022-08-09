2c387dd5e66dcbf2c70c0e831934ab0143ab9101
OB11TO <OB11TO61@yandex.ru>
2022-08-09 17:51:48 +0300

    Spring. 12. BeanFactoryPostProcessor (BFPP)

--- src/main/java/com/ob11to/spring/ApplicationRunner.java
+++ src/main/java/com/ob11to/spring/ApplicationRunner.java
@@ -5,12 +5,21 @@ import com.ob11to.spring.database.repository.CompanyRepository;
 import com.ob11to.spring.database.repository.UserRepository;
 import com.ob11to.spring.ioc.Container;
 import com.ob11to.spring.service.UserService;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             //        clazz -> String -> Map<String, Object>
             var connectionPool1 = context.getBean("p1", ConnectionPool.class);
