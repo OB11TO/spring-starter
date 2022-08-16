e4eee90fa941cd45452c4a04369c282bf42f95a4
OB11TO <OB11TO61@yandex.ru>
2022-08-16 16:18:26 +0300

    Spring. 24. Java-based Configuration

--- src/main/java/com/ob11to/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/ob11to/spring/config/ApplicationConfiguration.java
@@ -1,12 +1,15 @@
 package com.ob11to.spring.config;
 
 import com.ob11to.spring.database.repository.CrudRepository;
+import com.ob11to.web.config.WebConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.ob11to.spring")
