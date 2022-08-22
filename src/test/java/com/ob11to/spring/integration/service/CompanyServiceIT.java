9be2cb46b7fbeef05b86bc1317e5a2e2759d7306
OB11TO <OB11TO61@yandex.ru>
2022-08-22 17:13:54 +0300

    Spring. 42.43.44 Integration Testing

--- src/test/java/com/ob11to/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/ob11to/spring/integration/service/CompanyServiceIT.java
@@ -1,21 +1,23 @@
 package com.ob11to.spring.integration.service;
 
+import com.ob11to.spring.config.DataBaseProperties;
 import com.ob11to.spring.dto.CompanyReadDto;
+import com.ob11to.spring.integration.annotation.IT;
 import com.ob11to.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+    private final DataBaseProperties dataBaseProperties;
 
     @Test
     void findById(){
