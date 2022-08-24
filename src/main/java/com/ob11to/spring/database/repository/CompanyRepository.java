a84a322555fd813694af58a92188fb96a994afdc
OB11TO <OB11TO61@yandex.ru>
2022-08-24 18:12:26 +0300

    Spring. 54. RepositoryQuery

--- src/main/java/com/ob11to/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/ob11to/spring/database/repository/CompanyRepository.java
@@ -1,11 +1,11 @@
 package com.ob11to.spring.database.repository;
 
 import com.ob11to.spring.database.entity.Company;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CompanyRepository extends Repository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
     Optional<Company> findById(Integer id);
 
