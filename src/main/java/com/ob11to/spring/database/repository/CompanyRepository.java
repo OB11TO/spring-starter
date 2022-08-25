835c1cf489342d7515e96b5a6d358fc1dfab2516
OB11TO <OB11TO61@yandex.ru>
2022-08-25 17:03:35 +0300

    Spring. 56. NamedQuery

--- src/main/java/com/ob11to/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/ob11to/spring/database/repository/CompanyRepository.java
@@ -2,13 +2,14 @@ package com.ob11to.spring.database.repository;
 
 import com.ob11to.spring.database.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
-    Optional<Company> findByName(String name);
+    Optional<Company> findByName(@Param("name2")String name);
 
     List<Company> findAllByNameContainingIgnoreCase(String fragment);
 }
