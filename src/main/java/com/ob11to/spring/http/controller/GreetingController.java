af025758e1dc919db0d85019560b5c6ef2775ab1
OB11TO <OB11TO61@yandex.ru>
2022-09-07 15:48:28 +0300

    Spring. 79. Model

--- src/main/java/com/ob11to/spring/http/controller/GreetingController.java
+++ src/main/java/com/ob11to/spring/http/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.ob11to.spring.http.controller;
 
 import com.ob11to.spring.database.repository.CompanyRepository;
+import com.ob11to.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,23 +9,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
+
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+//        request.setAttribute(); requestScope
+//        request.getSession().setAttribute(); sessionScope
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+
+        return modelAndView;
+    }
 
     @GetMapping("/hello/{id}")
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView hello(ModelAndView modelAndView,
-                              HttpServletRequest request,
-                              CompanyRepository companyRepository,
-                              @RequestParam Integer age,
-                              @RequestHeader String accept,
-                              @CookieValue("JSESSIONID") String JSESSIONID,
-                              @PathVariable("id") Integer id) {
+    public ModelAndView hello2(ModelAndView modelAndView,
+                               HttpServletRequest request,
+                               CompanyRepository companyRepository,
+                               @RequestParam Integer age,
+                               @RequestHeader String accept,
+                               @CookieValue("JSESSIONID") String JSESSIONID,
+                               @PathVariable("id") Integer id) {
         var ageParam = request.getParameter("age");
         var acceptHeader = request.getHeader("accept");
         var cookies = request.getCookies();
@@ -34,7 +48,8 @@ public class GreetingController {
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye() {
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
+//      request.getSession().getAttribute("user");
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
 
