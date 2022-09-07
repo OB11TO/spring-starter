49ba59e797879c703e2be851002b70ba9f3b83d4
OB11TO <OB11TO61@yandex.ru>
2022-09-07 13:33:10 +0300

    Spring. 78. Parameters, Headers, Cookies

--- src/main/java/com/ob11to/spring/http/controller/GreetingController.java
+++ src/main/java/com/ob11to/spring/http/controller/GreetingController.java
@@ -2,8 +2,12 @@ package com.ob11to.spring.http.controller;
 
 import com.ob11to.spring.database.repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +16,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello/{id}")
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
+    public ModelAndView hello(ModelAndView modelAndView,
+                              HttpServletRequest request,
+                              CompanyRepository companyRepository,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String JSESSIONID,
+                              @PathVariable("id") Integer id) {
+        var ageParam = request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        var cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
