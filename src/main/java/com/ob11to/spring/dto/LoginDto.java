d9db5b85077f23dc3b74417631ddf087ad47d468
OB11TO <OB11TO61@yandex.ru>
2022-09-08 14:50:08 +0300

    Spring. 80. @ModelAttribute

---
+++ src/main/java/com/ob11to/spring/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.ob11to.spring.dto;
+
+import lombok.Value;
+
+@Value
+public class LoginDto {
+    String username;
+    String password;
+}
---
+++ src/main/java/com/ob11to/spring/http/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.ob11to.spring.http.controller;
+
+import com.ob11to.spring.dto.LoginDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(Model model, @ModelAttribute("login") LoginDto loginDto) {
+        return "user/login";
+    }
+}
