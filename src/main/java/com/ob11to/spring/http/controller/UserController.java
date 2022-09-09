1d0528abfb54cdbdbcaadfd229fc022eea020e8c
OB11TO <OB11TO61@yandex.ru>
2022-09-09 19:02:35 +0300

    Rest test

--- src/main/java/com/ob11to/spring/http/controller/UserController.java
+++ src/main/java/com/ob11to/spring/http/controller/UserController.java
@@ -1,15 +1,20 @@
-package com.ob11to.spring.service;
+package com.ob11to.spring.http.controller;
 
 import com.ob11to.spring.dto.UserCreateDto;
+import com.ob11to.spring.dto.UserReadDto;
+import com.ob11to.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
@@ -17,9 +22,9 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public String findAll(Model model) {
+    public ResponseEntity<List<UserReadDto>> findAll(Model model) {
 //        model.addAttribute("users", userService.findAll());
-        return "user/users";
+        return ResponseEntity.ok(userService.findAll());
     }
 
     @GetMapping("/{id}")
