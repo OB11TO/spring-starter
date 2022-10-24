2db960364d9f3e8a1cbf8d398427993c09ea597e
OB11TO <OB11TO61@yandex.ru>
2022-10-24 18:42:40 +0300

    Spring -> 102. Form Login НЕПОНЯТНО, КАК СДЕЛАТЬ КОНФИГ

---
+++ src/main/java/com/ob11to/spring/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.ob11to.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
