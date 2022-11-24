cea0345fbeec4002d0f82ef0406de86e119fe109
OB11TO <OB11TO61@yandex.ru>
2022-11-24 13:57:27 +0300

    Spring-_> 109. CSRF Filter

--- src/main/java/com/ob11to/spring/config/SecurityConfiguration.java
+++ src/main/java/com/ob11to/spring/config/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+//                .csrf().disable()
                 .authorizeHttpRequests(urlConfig -> urlConfig
                         .antMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .antMatchers("/users/{\\d+}/delete").hasAuthority(ADMIN.getAuthority())
